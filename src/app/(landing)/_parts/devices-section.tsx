5270f1830c0ec36c5986f893336131d2723a5db6
Adriano Maringolo <adrianomaringolo@gmail.com>
2025-09-17 17:30:10 -0300

    chore: activate new deploy

--- src/app/(landing)/_parts/devices-section.tsx
+++ src/app/(landing)/_parts/devices-section.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export const DeviceSection = () => {
 	return (
-		<section className="py-20">
+		<section className="py-20 ">
 			<div className="container mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="max-w-4xl mx-auto text-center">
 					<h2 className="text-3xl sm:text-4xl font-bold text-balance mb-6">
